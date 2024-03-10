a5448e719faf0ad883845a4a4cfeb9bbcb13d48b
2200090246 <2200090246@kluniversity.in>
2024-03-10 09:47:39 +0530

    first version

---
+++ frontend/src/components/nav.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+const Nav = () => {
+    const auth = localStorage.getItem('user');
+    const navigate = useNavigate();
+    const logout = () => {
+        localStorage.clear();
+        navigate('./signup')
+    }
+    return (
+        <div className="header">
+            <img src="https://static.vecteezy.com/system/resources/previews/009/029/391/non_2x/mws-logo-mws-letter-mws-letter-logo-design-initials-mws-logo-linked-with-circle-and-uppercase-monogram-logo-mws-typography-for-technology-business-and-real-estate-brand-vector.jpg" alt="logo" className="logo" />
+           
+            {
+                auth ?
+
+                    <ul className="nav-ul">
+                        <li><Link to='/'>Events</Link></li>
+                        <li><Link to='/add'>Add Events</Link></li>
+                        <li><Link to='/update'>Update Events</Link></li>
+                        <li><Link onClick={logout} to='/signup'>Logout ({JSON.parse(auth).name})</Link></li>
+                    </ul>
+                    :
+                    
+                    <ul className="nav-ul nav-right">
+                       
+                        <li className="sp"><Link to='/signup'>Sign Up</Link></li>
+                        <li><Link to='/login'>Login</Link></li>
+                    </ul>
+            }
+
+        </div>
+    )
+}
+
+export default Nav;
